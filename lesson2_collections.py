fe58584a0b86f865d03c5e4ce7bb94a9503405d4
SvitlanaPolishchuk3 <svitlana_polishchuk@epam.com>
2023-02-19 22:23:59 +0200

    some changes

--- lesson2_collections.py
+++ lesson2_collections.py
@@ -55,42 +55,15 @@ print(list_dicts)
 
 print(list_dicts[0].keys())
 print(list_dicts[0].values())
-#print(list_dicts[2].items())
 
 masterdict = {}
 for dict1 in list_dicts:
-    for el in dict1:
+    for el in dict1.keys():
         masterdict[el] = dict1[el]
-
 print(masterdict)
 
-# try:
-#     masterdict0 = dict.fromkeys(list_dicts[0].keys(), list_dicts[0].values())
-#     masterdict1 = dict.fromkeys(list_dicts[1].keys())
-#     masterdict2 = dict.fromkeys(list_dicts[2].keys())
-#     masterdict3 = dict.fromkeys(list_dicts[3].keys())
-#     masterdict4 = dict.fromkeys(list_dicts[4].keys())
-#     masterdict5 = dict.fromkeys(list_dicts[5].keys())
-#     masterdict6 = dict.fromkeys(list_dicts[6].keys())
-#     masterdict7 = dict.fromkeys(list_dicts[7].keys())
-#     masterdict8 = dict.fromkeys(list_dicts[8].keys())
-#     masterdict9 = dict.fromkeys(list_dicts[9].keys())
-#     masterdict10 = dict.fromkeys(list_dicts[9].keys())
-# except:
-#     pass
-# try:
-#     print(masterdict0)
-#     print(masterdict1)
-#     print(masterdict2)
-#     print(masterdict3)
-#     print(masterdict4)
-#     print(masterdict5)
-#     print(masterdict6)
-#     print(masterdict7)
-#     print(masterdict8)
-#     print(masterdict9)
-#     print(masterdict10)
-# except:
-#     pass
-#dict(masterdict1.items() | masterdict2.items())
-#print(dict)
+# valuestest = {}
+# for testvalue in list_dicts:
+#     if
+
+
