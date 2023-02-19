2141b2d64c8b09e05db49857f2499a080ddfa8a8
SvitlanaPolishchuk3 <svitlana_polishchuk@epam.com>
2023-02-19 21:10:48 +0200

    lesson 3 added

--- lesson2_collections.py
+++ lesson2_collections.py
@@ -7,52 +7,90 @@
 # 1. lets define number of dictionaries which we will use:
 import random
 import string
+
 dictnumber = random.randint(2, 10)
 print("Number of dictionaries: ", dictnumber)
 
 # This script will create expected number of dictionaries. This number was defined earlier:
-for i in range(dictnumber):
-    name = "dict_" + str(i)
-    exec(name + "={}")
-    print(name, "=", eval(name))
-    print(type(eval(name)))
-
-
 testlistkeys = random.randint(2, 10)
 print("Number of elements in dictionary: ", testlistkeys)
 
-random.seed(10)
 letters = string.ascii_lowercase
 keys1 = random.choices(letters, k=testlistkeys)
-
 print(keys1)
 
 values1 = random.sample(range(0, 100), testlistkeys)
 print(values1)
 
-# I could not find a proper solution to populate dictionaries dynamically using loop
-# so further solution is a bit separate
-# We need  to populate each dictionary:
+# Script to create one dictionary:
 dict_1 = {}
 for i in range(len(keys1)):
-   dict_1[keys1[i]] = values1[i]
+    dict_1[keys1[i]] = values1[i]
 print(dict_1)
 
-dict_2 = {}
-for j in range(len(keys1)):
-   dict_2[keys1[j]] = values1[j]
-print(dict_2)
+# Let`s create a new dictionary:
 
-# creating a list of those dictionaries:
-finallist = [dict_1, dict_2]
-print(finallist)
+list_dicts = []
+for i in range(dictnumber):
+    testlistkeys = random.randint(2, 10)
+    print("Number of elements in dictionary: ", testlistkeys)
+
+    letters = string.ascii_lowercase
+    keys1 = random.choices(letters, k=testlistkeys)
+    print(keys1)
+
+    values1 = random.sample(range(0, 100), testlistkeys)
+    print(values1)
+    dict_1 = {}
+    for i in range(len(keys1)):
+        dict_1[keys1[i]] = values1[i]
+    print(dict_1)
+    list_dicts.append(dict_1)
+print(list_dicts)
 
 # 2. get previously generated list of dicts and create one common dict:
 # if dicts have same key, we will take max value, and rename key with dict number with max value
 # if key is only in one dict - take it as is,
 # example: {'a_1': 5, 'b': 7, 'c': 35, 'g_2': 42}
 
-# Let`s create a new dictionary:
-masterdict = {x: dict_1[x] for x in keys1}
+print(list_dicts[0].keys())
+print(list_dicts[0].values())
+#print(list_dicts[2].items())
+
+masterdict = {}
+for dict1 in list_dicts:
+    for el in dict1:
+        masterdict[el] = dict1[el]
 
 print(masterdict)
+
+# try:
+#     masterdict0 = dict.fromkeys(list_dicts[0].keys(), list_dicts[0].values())
+#     masterdict1 = dict.fromkeys(list_dicts[1].keys())
+#     masterdict2 = dict.fromkeys(list_dicts[2].keys())
+#     masterdict3 = dict.fromkeys(list_dicts[3].keys())
+#     masterdict4 = dict.fromkeys(list_dicts[4].keys())
+#     masterdict5 = dict.fromkeys(list_dicts[5].keys())
+#     masterdict6 = dict.fromkeys(list_dicts[6].keys())
+#     masterdict7 = dict.fromkeys(list_dicts[7].keys())
+#     masterdict8 = dict.fromkeys(list_dicts[8].keys())
+#     masterdict9 = dict.fromkeys(list_dicts[9].keys())
+#     masterdict10 = dict.fromkeys(list_dicts[9].keys())
+# except:
+#     pass
+# try:
+#     print(masterdict0)
+#     print(masterdict1)
+#     print(masterdict2)
+#     print(masterdict3)
+#     print(masterdict4)
+#     print(masterdict5)
+#     print(masterdict6)
+#     print(masterdict7)
+#     print(masterdict8)
+#     print(masterdict9)
+#     print(masterdict10)
+# except:
+#     pass
+#dict(masterdict1.items() | masterdict2.items())
+#print(dict)
---
+++ lesson3_string.py
@@ -0,0 +1,57 @@
+import re
+a = '''homEwork:
+  tHis iz your homeWork, copy these Text to variable.
+  You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
+  it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
+  last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87.'''
+
+# Let`s apply Capitalize. It will convert all letters to lowercase except for the first letter in the text
+b = (a.capitalize())
+print(b)
+
+# Let`s replace 'iz' with 'is'. For pattern to work correct I add a space before iz:
+c = (b.replace(' iz', ' is'))
+print(c)
+
+# My problem now that I have my sentences starting with lowercase. This script will split text in parts,
+# then capitalize each sentence and after that join sentences in one text again.
+sentences = c.split('.')
+final_senteces = []
+for sentence in sentences:
+    sentence = sentence.strip()
+    if not sentence:
+        continue
+    sentence = sentence.capitalize()
+    final_senteces.append(sentence)
+final_text = '. '.join(final_senteces)
+final_text = final_text+'.'
+print(final_text)
+
+# Now we need to create another sentence with last words of every sentence.
+# This script will find all words that are followed by '.'
+pattern = r'\w+\.'
+test1 = (re.findall(pattern, final_text))
+print(test1)
+
+# This script will change our list back into string
+test2 = ' '.join(test1)
+print(test2)
+
+# Now we can replace '.' with spaces and capitalize again.
+d = (test2.replace('. ', ' '))
+e = (d.capitalize())
+print(e)
+
+# Now we can join main text and our last sentence.
+final_text2 = final_text + ' ' + e
+print(final_text2)
+
+# We can use unicode symbol of whitespace to find all of them in the text.
+testcount = (re.findall('\u0020', final_text2))
+
+# Now we calculate them using 'len'
+testcount1 = len(testcount)
+print(testcount1)
+
+
+
