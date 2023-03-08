2759503f1aca1ea12c15d807451519da83ed2fab
SvitlanaPolishchuk3 <svitlana_polishchuk@epam.com>
2023-03-08 18:52:48 +0200

    lesson5 updates

--- lesson5_Classes.py
+++ lesson5_Classes.py
@@ -1,14 +1,3 @@
-# class News:
-#      f = open("guru99.txt", "w+")
-#      def __init__(self, name):
-#           self.name = name
-#
-# # news1 = News('News----')
-#      f.write(News('News----'))
-#      f.close()
-# print(news1.name)
-
-
 from datetime import datetime
 from datetime import date
 import tkinter as tk
@@ -18,9 +7,9 @@ import tkinter as tk
 now = datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M")
 
-def countdaysleft():
-    expiredate = (date(2023, 3, 29) - date.today()).days
-    return expiredate
+# def countdaysleft():
+#     expiredate = (date(2023, 3, 29) - date.today()).days
+#     return expiredate
 
 
 # print(str(countdaysleft())+" days left")
@@ -28,7 +17,7 @@ def countdaysleft():
 
 class Newsblock:
     def __init__(self, news1, city):
-        self.intro = "News -------------------------"
+        self.intro = "\nNews -------------------------"
         self.news1 = news1
         self.city = city
         self.time1 = dt_string
@@ -37,7 +26,7 @@ class Newsblock:
     def __str__(self):
         return f"{self.intro}\n{self.news1}\n{self.city}, {self.time1}\n{self.ending}"
     def publish_smth(self):
-        f = open("Newsfeed.txt", "a")
+        f = open("Newsfeed.txt", "w+")
         f.write(f"{self.intro}\n{self.news1}\n{self.city}, {self.time1}\n{self.ending}")
         f.close()
 
@@ -50,7 +39,6 @@ class Ad:
     def __init__(self, intro, adtext, ending, year, month, day):
         self.intro = intro
         self.adtext = adtext
-        self.time1 = countdaysleft()
         self.ending = ending
         self.year = year
         self.month = month
@@ -63,21 +51,33 @@ class Ad:
     def __str__(self):
         return f"{self.intro}\n{self.adtext}\n{self.countdaysleft()}\n{self.ending}"
     def publish_smth(self):
-        f = open("Newsfeed.txt", "a")
-        f.write(f"Actual until:{self.intro}\n{self.adtext}\n{self.countdaysleft()} days left \n{self.ending}")
+        f = open("Newsfeed.txt", "w+")
+        f.write(f"{self.intro}\n{self.adtext}\nActual until: {self.day}/{self.month}/{self.year}, {self.countdaysleft()} days left \n{self.ending}")
         f.close()
 
 
 # q = Ad("Ad ---------------------------","This is ad about real estate", "------------------------------")
 # q.publish_smth()
 # print(q)
+class Weather:
+    def __init__(self, weather1, weather2, weather3):
+        self.intro = "\nWeather forecast-------------"
+        self.weather1 = weather1
+        self.weather2 = weather2
+        self.weather3 = weather3
+        self.ending = "------------------------------"
 
+    def __str__(self):
+        return f"{self.intro}\n{self.weather1}\n{self.weather2}{self.weather3}\n{self.ending}"
+    def publish_smth(self):
+        f = open("Newsfeed.txt", "w+")
+        f.write(f"{self.intro}\n{self.weather1}\n{self.weather2}\n{self.weather3}\n{self.ending}")
+        f.close()
 
 
 while True:
-    # f = open("Newsfeed.txt", "w")
-    with open('Newsfeed.txt', 'w') as file:
-        file.write("News feed:\n")
+    # with open('Newsfeed.txt', 'w') as file:
+    #     file.write("News feed:\n")
     print("Main menu")
     print("Press 1 for News")
     print("Press 2 for Ad")
@@ -99,15 +99,17 @@ while True:
         year=int(input("Print expire year here ="))
         month = int(input("Print expire month here ="))
         day = int(input("Print expire day here ="))
-        p2 = Ad("nnn", ad1, "gggg", year, month, day)
+        p2 = Ad("\n\nPrivate Ad ------------------", ad1, "------------------------------", year, month, day)
         p2.publish_smth()
 
 
     elif (choice==3):
         print("Write a weather forecast in console:")
-        choiceweather = tk.Tk()
-        e = tk.Entry(choiceweather)
-        choiceweather.mainloop()
+        weath1 = input("Print weather for today in Ukraine =")
+        weath2 = input("Print weather for 7 days in Ukraine =")
+        weath3 = input("Print weather for other countries =")
+        r = Weather(weath1, weath2, weath3)
+        r.publish_smth()
 
     elif (choice==9):
         break
